a4d1d49c27a62b485aa77cc0670118eed06524cf
MaxUlysse <max@ithake.eu>
2017-05-04 10:33:11 +0200

    update search.js

--- js/search/search.js
+++ js/search/search.js
@@ -1,86 +1,234 @@
-(function() {
-  function displaySearchResults(results, store) {
-    var searchResults = document.getElementById('search-results');
+/**
+ * A simple JSON search
+ * Requires jQuery (v 1.7+)
+ *
+ * @author   Mat Hayward - Erskine Design
+ * @author   Rishikesh Darandale <Rishikesh.Darandale@gmail.com>
+ * @version  1.1
+ */
 
-    if (results.length) { // Are there any results?
-      var appendString = '';
+ /* ==========================================================================
+    Initialisation
+    ========================================================================== */
 
-      for (var i = 0; i < results.length; i++) {  // Iterate over the results
-        var item = store[results[i].ref];
+var q, jsonFeedUrl = "/feeds/feed.json",
+  $searchForm = $("[data-search-form]"),
+  $searchInput = $("[data-search-input]"),
+  $resultTemplate = $("#search-result"),
+  $resultsPlaceholder = $("[data-search-results]"),
+  $foundContainer = $("[data-search-found]"),
+  $foundTerm = $("[data-search-found-term]"),
+  $foundCount = $("[data-search-found-count]"),
+  allowEmpty = true,
+  showLoader = true,
+  loadingClass = "is--loading",
+  indexVar;
 
-        if (i == 0) {
-          appendString += '<div class="card-columns">';
-        }
 
-        appendString += '<div class="card">';
-        appendString += '<img class="card-img-top img-fluid" src="' + item.image + '" alt="' + item.title + '" title="' + item.title + '"">';
-        appendString += '<div class="card-block">';
-        appendString += '<h4 class="card-title"><a href="' + item.url + '">' + item.title + '</a></h4>';
-        appendString += '<p class="card-text">' + item.description + '</p>';
-        appendString += '<p class="text-center"><a href="' + item.url + '" class="btn btn-primary">Read More</a></p>';
-        appendString += '<nav class="nav">';
-        appendString += '<div class="nav-link"><i class="fa fa-calendar" aria-hidden="true"></i><small class="text-muted"> ' + item.date + '</small></div>';
-        appendString += '<div class="nav-link"><i class="fa fa-tags" aria-hidden="true"></i><small class="text-muted"> ' + item.tags + '</small></div>'
-        appendString += '</nav>';
-        appendString += '</div>';
-        appendString += '</div>';
+$(document).ready( function() {
+  // hide items found string
+  $foundContainer.hide();
+  // initiate search functionality
+  initSearch();
+});
 
-        if (i == results.length-1) {
-          appendString += '</div>';
-        }
-      }
+ /* ==========================================================================
+    Search functions
+    ========================================================================== */
 
-      searchResults.innerHTML = appendString;
-    } else {
-      searchResults.innerHTML = '<li>No results found</li>';
+/**
+ * Initiate search functionality.
+ * Shows results based on querystring if present.
+ * Binds search function to form submission.
+ */
+function initSearch() {
+  if(!sessionStorage.getItem("lunrIndex")) {
+    // get the data
+    getData();
+  } else {
+    // Get search results if q parameter is set in querystring
+    if (getParameterByName('q')) {
+      q = decodeURIComponent(getParameterByName('q'));
+      $searchInput.val(q);
+      execSearch(q);
     }
   }
 
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
+  // Get search results on submission of form
+  $(document).on("submit", $searchForm, function(e) {
+    e.preventDefault();
+    q = $searchInput.val();
+    execSearch(q);
+  });
+}
 
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
-
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-      }
-    }
-  }
-
-  var searchTerm = getQueryVariable('query');
-
-  if (searchTerm) {
-    document.getElementById('search-box').setAttribute("value", searchTerm);
-
-    // Initalize lunr with the fields it will be searching on. I've given title
-    // a boost of 10 to indicate matches on this field are more important.
-    var idx = lunr(function () {
-      this.field('id');
-      this.field('title', { boost: 10 });
-      this.field('description');
-      this.field('date');
-      this.field('author');
-      this.field('tags');
-      this.field('image');
-      this.field('content');
-    });
-
-    for (var key in window.store) { // Add the data to lunr
-      idx.add({
-        'id': key,
-        'title': window.store[key].title,
-        'description': window.store[key].description,
-        'date': window.store[key].date,
-        'author': window.store[key].author,
-        'tags': window.store[key].tags,
-        'image': window.store[key].image,
-        'content': window.store[key].content
+/**
+ * Get the JSON data
+ * Get the generated feeds/feed.json file so lunr.js can search it locally.
+ * Store the index in sessionStorage
+ */
+function getData(indexVar) {
+  jqxhr = $.getJSON(jsonFeedUrl)
+    .done(function(loaded_data){
+      // save the actual data as well
+      sessionStorage.setItem("actualData", JSON.stringify(loaded_data));
+      // set the index fields
+      indexVar = lunr(function () {
+        this.field('id');
+        this.field('title');
+        this.field('content', { boost: 10 });
+        this.field('author');
+        loaded_data.forEach(function (doc, index) {
+          if ( doc.search_omit != "true" ) {
+            console.log("adding to index: " + doc.title);
+            this.add($.extend({ "id": index }, doc));
+          }
+        }, this)
       });
+      // store the index in sessionStorage
+      sessionStorage.setItem("lunrIndex", JSON.stringify(indexVar));
+      // Get search results if q parameter is set in querystring
+      if (getParameterByName('q')) {
+        q = decodeURIComponent(getParameterByName('q'));
+        $searchInput.val(q);
+        execSearch(q);
+      }
+    })
+    .fail( function() {
+      console.log("get json failed...");
+    })
+    .always( function() {
+      console.log("finally...");
+    });
+}
 
-      var results = idx.search(searchTerm); // Get lunr to perform a search
-      displaySearchResults(results, window.store); // We'll write this in the next section
+/**
+ * Get the search result from lunr
+ * @param {String} q
+ * @returns search results
+ */
+function getResults(q) {
+  var savedIndexData = JSON.parse(sessionStorage.getItem("lunrIndex"));
+  console.log("Indexed var from sessionStorage: " + savedIndexData);
+  return lunr.Index.load(savedIndexData).search(q);
+}
+
+/**
+ * Executes search
+ * @param {String} q
+ * @return null
+ */
+function execSearch(q) {
+  if (q != '' || allowEmpty) {
+    if (showLoader) {
+      toggleLoadingClass();
     }
+    processResultData(getResults(q));
   }
-})();
+}
+
+/**
+ * Toggles loading class on results and found string
+ * @return null
+ */
+function toggleLoadingClass() {
+  $resultsPlaceholder.toggleClass(loadingClass);
+  $foundContainer.toggleClass(loadingClass);
+}
+
+/**
+ * Process search result data
+ * @return null
+ */
+function processResultData(searchResults) {
+  $results = [];
+
+  console.log("Search Results: " + searchResults);
+  var resultsCount = 0,
+      results = "";
+
+  // Iterate over the results
+  searchResults.forEach(function(result) {
+    var loaded_data = JSON.parse(sessionStorage.getItem("actualData"));
+    var item = loaded_data[result.ref];
+    var result = populateResultContent($resultTemplate.html(), item);
+        resultsCount++;
+        results += result;
+  });
+
+
+  if (showLoader) {
+    toggleLoadingClass();
+  }
+
+  populateResultsString(resultsCount);
+  showSearchResults(results);
+}
+
+/**
+ * Add search results to placeholder
+ * @param {String} results
+ * @return null
+ */
+function showSearchResults(results) {
+  // Add results HTML to placeholder
+  $resultsPlaceholder.html(results);
+}
+
+/**
+ * Add results content to item template
+ * @param {String} html
+ * @param {object} item
+ * @return {String} Populated HTML
+ */
+function populateResultContent(html, item) {
+  html = injectContent(html, item.title, '##Title##');
+  html = injectContent(html, item.link, '##Url##');
+  if(item.excerpt)
+    html = injectContent(html, item.excerpt, '##Excerpt##');
+  else
+    html = injectContent(html, "", '##Excerpt##');
+  if( item.date)
+    html = injectContent(html, item.date, '##Date##');
+  else
+    html = injectContent(html, "", '##Date##');
+  return html;
+}
+
+/**
+ * Populates results string
+ * @param {String} count
+ * @return null
+ */
+function populateResultsString(count) {
+  $foundTerm.text(q);
+  $foundCount.text(count);
+  $foundContainer.show();
+}
+
+ /* ==========================================================================
+    Helper functions
+    ========================================================================== */
+
+
+/**
+ * Gets query string parameter - taken from http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+ * @param {String} name
+ * @return {String} parameter value
+ */
+function getParameterByName(name) {
+  var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+/**
+ * Injects content into template using placeholder
+ * @param {String} originalContent
+ * @param {String} injection
+ * @param {String} placeholder
+ * @return {String} injected content
+ */
+function injectContent(originalContent, injection, placeholder) {
+  var regex = new RegExp(placeholder, 'g');
+  return originalContent.replace(regex, injection);
+}
