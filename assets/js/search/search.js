137d1ba9c921e689c31d9060fa73eb54851396dc
Maxime Garcia <max.u.garcia@gmail.com>
2017-09-29 12:43:19 +0200

    update layout and fix search

--- assets/js/search/search.js
+++ assets/js/search/search.js
@@ -11,7 +11,7 @@
 Initialisation
 ========================================================================== */
 
-var q, jsonFeedUrl = "/feeds/feed.json",
+var q, jsonFeedUrl = "/assets/feed/feed.json",
   $searchForm = $("[data-search-form]"),
   $searchInput = $("[data-search-input]"),
   $resultTemplate = $("#search-result"),
