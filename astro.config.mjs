dd300f2940d884a13b72a275587f46415eee8e5f
Maxime U. Garcia <max.u.garcia@gmail.com>
2026-05-26 13:44:45 +0200

    Remove unused SCSS preprocessor config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://maxulysse.github.io',
   output: 'static',
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler'
-        }
-      }
-    }
-  }
 });
