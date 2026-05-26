9dbb089e30eba59d4c64e3647e9fd14f2c39b476
Maxime U Garcia <max.u.garcia@gmail.com>
2026-05-26 15:40:26 +0200

    Add XML sitemap via @astrojs/sitemap (#335)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://maxulysse.github.io',
   output: 'static',
+  integrations: [sitemap()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
+      }
+    }
+  }
 });
