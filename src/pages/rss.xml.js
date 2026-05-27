22cbda518c7d2581724d5231638074d7f1706041
Maxime U Garcia <max.u.garcia@gmail.com>
2026-05-27 11:24:18 +0200

    Fix RSS feed link using post.id instead of post.slug (#359)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -16,7 +16,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description || post.body.substring(0, 150),
-      link: `/blog/${post.slug}/`,
+      link: `/blog/${post.id}/`,
     })),
   });
 }
