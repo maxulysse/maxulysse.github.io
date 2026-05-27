4d2829cbbd5a40cf6cb56bc74903b27054051fd3
Maxime U Garcia <max.u.garcia@gmail.com>
2026-05-27 11:25:12 +0200

    Remove orphaned Vite SCSS preprocessor config (#362)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,4 @@ export default defineConfig({
   site: 'https://maxulysse.github.io',
   output: 'static',
   integrations: [sitemap()],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler'
-        }
-      }
-    }
-  }
 });
